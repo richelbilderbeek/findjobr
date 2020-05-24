8afe40a875ce0b051a1844813b578ccd28d14e9f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-24 12:08:36 +0200

    Look for open access articles

--- R/find_best_jobs.R
+++ R/find_best_jobs.R
@@ -1,5 +1,13 @@
-#' Find the best jobs for me
+#' Find the best jobs for me.
 #' @export
 find_best_jobs <- function() {
-  tibble::tibble()
+  random_dois <- rcrossref::cr_r(sample = 10)
+
+  df <- purrr::map(random_dois, .f = purrr::safely(
+    function(x) roadoi::oadoi_fetch(x, email = "richel@richelbilderbeek.nl")
+  )) %>% purrr::map_df("result")
+
+  # Only use open access aricles
+  df <- df[ df$is_oa == TRUE, ]
+  df
 }
--- tests/testthat/test-find_best_jobs.R
+++ tests/testthat/test-find_best_jobs.R
@@ -1,4 +1,5 @@
 test_that("use", {
   df <- find_best_jobs()
   expect_true(tibble::is_tibble(df))
+  expect_true(nrow(df) > 0)
 })
